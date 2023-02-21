46f1a20daa77f720c6555faee6cd5325784d55ed
Vetiver <99443703+Vetiver@users.noreply.github.com>
2023-02-21 14:19:18 +0300

    fibonacci

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState, useMemo, useEffect} from "react";
+import style from '../fibonacci-page/fibonacci-page.module.css';
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const FibonacciPage: React.FC = () => {
+  const [input, setInput] = useState<any>();
+  const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setInput([e.target.value]);
+    
+  };
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <div className={style.mainContainer}>
+        <div className={style.container}>
+          <Input
+            onChange={onChange}
+            type="number"
+            max={19}
+            isLimitText={true}
+          />
+          <Button  text="Рассчитать"/>
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
